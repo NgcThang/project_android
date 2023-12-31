1de52f7be39baa5507ef043b8c40de8e394c876b
NgcThang <dgngocthang1410@gmail.com>
2023-12-31 20:57:29 +0700

    upgrade tuan 3

---
+++ app/src/main/java/tuan3/Tuan32.java
@@ -0,0 +1,36 @@
+package tuan3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import com.example.projecta.R;
+
+import java.util.ArrayList;
+
+public class Tuan32 extends AppCompatActivity {
+    ListView lv;
+    T32Adapter adapter;
+    ArrayList<T32Contact> ls=new ArrayList<>();
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuan32);
+        lv= findViewById(R.id.tuan32Lv);
+        //tao nguon dulieu
+        ls.add(new T32Contact("Nguyen van a","18",R.drawable.ic_launcher_background));
+        ls.add(new T32Contact("Tran van b","16",R.drawable.ic_launcher_background));
+        ls.add(new T32Contact("Vu van c","17",R.drawable.ic_launcher_background));
+        ls.add(new T32Contact("Nguyen thi d","22",R.drawable.ic_launcher_background));
+        ls.add(new T32Contact("Nguyen van y","32",R.drawable.ic_launcher_background));
+        ls.add(new T32Contact("Nguyen van x","11",R.drawable.ic_launcher_background));
+        //tao adapter
+        adapter = new T32Adapter(ls,this);
+        lv.setAdapter(adapter);
+    }
+}
